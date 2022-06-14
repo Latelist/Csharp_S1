a3b92a129f22e3b4205921636c9ae9235c0966bc
Salatchen <pozhivi.s.moe@gmail.com>
2022-06-14 17:15:13 +0300

    Exercise001 Squared Number mistake + .gitignore

---
+++ Exercise001_SquareNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая выдаёт квадрат числа
+Console.Write("Введите число: ");
+
+int n = Console.ReadLine();
+int result;
+
+Console.WriteLine(n);
+
+result = n * n;
+
+Console.WriteLine(result);
